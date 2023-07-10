d62e1b300ce4d055a3e6521bc0abbd1e4947dbc1
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-10 23:54:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,72 +1,88 @@
 import streamlit as st
 import PyPDF2
 import re
-import requests
 
 def load_resume_text():
-    pdf_url = 'https://github.com/Rishika631/resume/blob/main/Rishika_Agrawal_resumeofficial.pdf'  # Update with the URL to your resume PDF file
+    pdf_path = 'path/to/your/resume.pdf'  # Update with the path to your resume PDF file
     resume_text = ""
 
-    response = requests.get(pdf_url)
-    with open('temp_resume.pdf', 'wb') as file:
-        file.write(response.content)
-
-    with open('temp_resume.pdf', 'rb') as file:
+    with open(pdf_path, 'rb') as file:
         reader = PyPDF2.PdfReader(file)
         num_pages = len(reader.pages)
-
+        
         for page_num in range(num_pages):
             page = reader.pages[page_num]
             resume_text += page.extract_text()
 
     return resume_text
 
-def extract_links(resume_text):
-    # Find all URLs or hyperlinks in the resume text
-    pattern = r"(?P<url>https?://[^\s]+)"
-    matches = re.findall(pattern, resume_text)
-    links = list(set(matches))  # Remove duplicates if any
-    return links
+def extract_experience(resume_text):
+    experience_section = ""
+    experience_pattern = r"EXPERIENCE[\s\S]*?(?=ACHIEVEMENTS|EDUCATION|SKILLS\sSUMMARY|PROJECTS|\Z)"
+    matches = re.findall(experience_pattern, resume_text, re.IGNORECASE)
+    if matches:
+        experience_section = matches[0].strip()
+    return experience_section
 
+def extract_achievements(resume_text):
+    achievements_section = ""
+    achievements_pattern = r"ACHIEVEMENTS[\s\S]*?(?=EXPERIENCE|EDUCATION|SKILLS\sSUMMARY|PROJECTS|\Z)"
+    matches = re.findall(achievements_pattern, resume_text, re.IGNORECASE)
+    if matches:
+        achievements_section = matches[0].strip()
+    return achievements_section
 
-def generate_response(message):
-    # Extract the links from the resume text
-    links = extract_links(resume_text)
+def extract_education(resume_text):
+    education_section = ""
+    education_pattern = r"EDUCATION[\s\S]*?(?=EXPERIENCE|ACHIEVEMENTS|SKILLS\sSUMMARY|PROJECTS|\Z)"
+    matches = re.findall(education_pattern, resume_text, re.IGNORECASE)
+    if matches:
+        education_section = matches[0].strip()
+    return education_section
 
-    # Process user input and generate response
+def extract_skills_summary(resume_text):
+    skills_summary_section = ""
+    skills_summary_pattern = r"SKILLS\sSUMMARY[\s\S]*?(?=EXPERIENCE|ACHIEVEMENTS|EDUCATION|PROJECTS|\Z)"
+    matches = re.findall(skills_summary_pattern, resume_text, re.IGNORECASE)
+    if matches:
+        skills_summary_section = matches[0].strip()
+    return skills_summary_section
+
+def extract_projects(resume_text):
+    projects_section = ""
+    projects_pattern = r"PROJECTS[\s\S]*?(?=EXPERIENCE|ACHIEVEMENTS|EDUCATION|SKILLS\sSUMMARY|\Z)"
+    matches = re.findall(projects_pattern, resume_text, re.IGNORECASE)
+    if matches:
+        projects_section = matches[0].strip()
+    return projects_section
+
+def generate_response(message, resume_text):
     response = ""
 
     if "experience" in message.lower():
-        # Generate response about experience
-        response = "Here is my experience: ..."
+        # Extract and generate response about experience
+        experience_section = extract_experience(resume_text)
+        response = f"Experience:\n{experience_section}"
 
-    elif "projects" in message.lower():
-        # Generate response about projects
-        response = "Here are my projects: ..."
-
-    elif "skills" in message.lower():
-        # Generate response about skills
-        response = "Here are my skills: ..."
+    elif "achievements" in message.lower():
+        # Extract and generate response about achievements
+        achievements_section = extract_achievements(resume_text)
+        response = f"Achievements:\n{achievements_section}"
 
     elif "education" in message.lower():
-        # Generate response about education
-        response = "Here is my education: ..."
+        # Extract and generate response about education
+        education_section = extract_education(resume_text)
+        response = f"Education:\n{education_section}"
 
-    elif any(keyword in message.lower() for keyword in ["github", "code", "repository"]):
-        # Check if user is asking for GitHub repository
-        github_links = [link for link in links if "github" in link]
-        if github_links:
-            response = f"Here is my GitHub repository: {github_links[0]}"
-        else:
-            response = "I don't have a GitHub repository available."
+    elif "skills" in message.lower() or "summary" in message.lower():
+        # Extract and generate response about skills summary
+        skills_summary_section = extract_skills_summary(resume_text)
+        response = f"Skills Summary:\n{skills_summary_section}"
 
-    elif "linkedin" in message.lower():
-        # Check if user is asking for LinkedIn profile
-        linkedin_links = [link for link in links if "linkedin" in link]
-        if linkedin_links:
-            response = f"Here is my LinkedIn profile: {linkedin_links[0]}"
-        else:
-            response = "I don't have a LinkedIn profile available."
+    elif "projects" in message.lower():
+        # Extract and generate response about projects
+        projects_section = extract_projects(resume_text)
+        response = f"Projects:\n{projects_section}"
 
     else:
         response = "I'm sorry, I couldn't understand your question."
@@ -80,22 +96,18 @@ def main():
     # Load the resume text from the PDF
     resume_text = load_resume_text()
 
-    # Display the resume text
-    st.subheader("Resume Details")
-    st.write(resume_text)
-
     # Chatbot conversation loop
     user_input = st.text_input("You:")
     chat_history = []
 
     if user_input:
         chat_history.append(user_input)
-        response = generate_response(user_input)
+        response = generate_response(user_input, resume_text)
         chat_history.append(response)
 
         # Display the chat history
         st.subheader("Chat History")
-        for i in range(0, len(chat_history), 2):
+        for i in range(0, len(chat_history), 3):
             st.write("You: " + chat_history[i])
             st.write("Chatbot: " + chat_history[i + 1])
 
