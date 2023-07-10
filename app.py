461ea0328ce08bf097186bdf05ddba9c3effb39e
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-10 23:38:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,120 +1,53 @@
-import re
 import streamlit as st
-import openai
-import PyPDF2
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
 
-# Set up OpenAI API
-openai.api_key = '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'
-
-def load_pdf_content():
-    pdf_path = 'Rishika_Agrawal_resumeofficial.pdf'  # Update with the path to your PDF file
-    pdf_content = ""
-
-    with open(pdf_path, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
-        num_pages = len(reader.pages)
-        
-        for page_num in range(num_pages):
-            page = reader.pages[page_num]
-            pdf_content += page.extract_text()
-
-    return pdf_content
-
-def generate_chatbot_response(message):
-    response = openai.Completion.create(
-        engine='text-davinci-003',
-        prompt=f"You: {message}\nAssistant:",
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.7
-    )
-    return response.choices[0].text.strip()
-    
-def extract_experience(resume_content):
-    experience_section = ""
-    # Find the experience section based on a specific pattern in the resume content
-    pattern = r"EXPERIENCE(.*?)EDUCATION"
-    match = re.search(pattern, resume_content, re.IGNORECASE | re.DOTALL)
-    if match:
-        experience_section = match.group(1).strip()
-    return experience_section
-
-def extract_achievements(resume_content):
-    achievements_section = ""
-    # Find the achievements section based on a specific pattern in the resume content
-    pattern = r"ACHIEVEMENTS & CERTIFICATES(.*?)EXPERIENCE"
-    match = re.search(pattern, resume_content, re.IGNORECASE | re.DOTALL)
-    if match:
-        achievements_section = match.group(1).strip()
-    return achievements_section
-
-def extract_links(resume_content):
-    links = []
-    # Find all the hyperlinks in the resume content using regular expression pattern matching
-    pattern = r"\[(.*?)\]\((.*?)\)"
-    matches = re.findall(pattern, resume_content)
-    for match in matches:
-        link_text, link_url = match
-        links.append((link_text, link_url))
-    return links
-
-def generate_response(message, resume_content):
-    response = ""
-    
-    # Check if the message is related to the resume content
-    if "experience" in message.lower():
-        # Extract the experience section from the resume content
-        experience_section = extract_experience(resume_content)
-        response = "Here is Rishika's experience:\n" + experience_section
-        
-    elif "achievements" in message.lower():
-        # Extract the achievements section from the resume content
-        achievements_section = extract_achievements(resume_content)
-        response = "Here are Rishika's achievements:\n" + achievements_section
-        
-    elif "linkedin" in message.lower():
-        # Extract the LinkedIn profile link from the resume content
-        links = extract_links(resume_content)
-        linkedin_links = [(text, url) for (text, url) in links if "linkedin" in url]
-        if linkedin_links:
-            response = "Here is Rishika's LinkedIn profile:\n"
-            for text, url in linkedin_links:
-                response += f"- [{text}]({url})\n"
-        else:
-            response = "Rishika's LinkedIn profile link is not available."
-    
-    else:
-        response = generate_chatbot_response(message)  # Replace with your chatbot's logic
-        
-    return response
-
+# streamlit run app.py
 
 def main():
-    st.title("Conversational Form Chatbot")
-    st.write("Welcome! Start a conversation with the chatbot.")
+    load_dotenv()
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Ask your PDF 💬")
 
-    # Load PDF content
-    pdf_content = load_pdf_content()
+    # Specify the PDF path from the repository
+    pdf_path = 'https://raw.githubusercontent.com/your-username/your-repository/main/your-pdf-file.pdf'
 
-    # Display the PDF content
-    st.subheader("Resume PDF Content")
-    st.write(pdf_content)
+    # Load the PDF content from the specified path
+    pdf = PdfReader(pdf_path)
+    text = ""
+    for page in pdf.pages:
+        text += page.extract_text()
 
-    # Chatbot conversation loop
-    user_input = st.text_input("You:")
-    chat_history = []
+    # Split the text into chunks
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
 
-    if user_input:
-        chat_history.append(user_input)
-        response = generate_response(user_input, pdf_content)
-        chat_history.append(response)
+    # Create embeddings
+    embeddings = OpenAIEmbeddings()
+    knowledge_base = FAISS.from_texts(chunks, embeddings)
 
-        # Display the chat history
-        st.subheader("Chat History")
-        for i in range(0, len(chat_history), 2):
-            st.write("You: " + chat_history[i])
-            st.write("Chatbot: " + chat_history[i + 1])
+    # Show user input
+    user_question = st.text_input("Ask a question about your PDF:")
+    if user_question:
+        docs = knowledge_base.similarity_search(user_question)
+
+        llm = OpenAI()
+        chain = load_qa_chain(llm, chain_type="stuff")
+        with get_openai_callback() as cb:
+            response = chain.run(input_documents=docs, question=user_question)
+            print(cb)
+
+        st.write(response)
 
 if __name__ == "__main__":
     main()
