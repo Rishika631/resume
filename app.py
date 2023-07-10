205faacccc9a3add8e8669492b4365c544468a07
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-10 23:45:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,53 +1,98 @@
 import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.callbacks import get_openai_callback
+import PyPDF2
+import re
 
-# streamlit run app.py
-openai.api_key = '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'
+def load_resume_text():
+    pdf_path = 'path/to/your/resume.pdf'  # Update with the path to your resume PDF file
+    resume_text = ""
+
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        num_pages = len(reader.pages)
+        
+        for page_num in range(num_pages):
+            page = reader.pages[page_num]
+            resume_text += page.extract_text()
+
+    return resume_text
+
+def extract_links(resume_text):
+    # Find all URLs or hyperlinks in the resume text
+    pattern = r"(?P<url>https?://[^\s]+)"
+    matches = re.findall(pattern, resume_text)
+    links = list(set(matches))  # Remove duplicates if any
+    return links
+
+
+def generate_response(message):
+    # Extract the links from the resume text
+    links = extract_links(resume_text)
+
+    # Process user input and generate response
+    response = ""
+
+    if "experience" in message.lower():
+        # Generate response about experience
+        response = "Here is my experience: ..."
+
+    elif "projects" in message.lower():
+        # Generate response about projects
+        response = "Here are my projects: ..."
+
+    elif "skills" in message.lower():
+        # Generate response about skills
+        response = "Here are my skills: ..."
+
+    elif "education" in message.lower():
+        # Generate response about education
+        response = "Here is my education: ..."
+
+    elif any(keyword in message.lower() for keyword in ["github", "code", "repository"]):
+        # Check if user is asking for GitHub repository
+        github_links = [link for link in links if "github" in link]
+        if github_links:
+            response = f"Here is my GitHub repository: {github_links[0]}"
+        else:
+            response = "I don't have a GitHub repository available."
+
+    elif "linkedin" in message.lower():
+        # Check if user is asking for LinkedIn profile
+        linkedin_links = [link for link in links if "linkedin" in link]
+        if linkedin_links:
+            response = f"Here is my LinkedIn profile: {linkedin_links[0]}"
+        else:
+            response = "I don't have a LinkedIn profile available."
+
+    else:
+        response = "I'm sorry, I couldn't understand your question."
+
+    return response
+
 def main():
-    load_dotenv()
-    st.set_page_config(page_title="Ask your PDF")
-    st.header("Ask your PDF 💬")
+    st.title("Resume Chatbot")
+    st.write("Welcome! Start a conversation with the chatbot.")
 
-    # Specify the PDF path from the repository
-    pdf_path = 'https://github.com/Rishika631/resume/blob/main/Rishika_Agrawal_resumeofficial.pdf'
+    # Load the resume text from the PDF
+    resume_text = load_resume_text()
 
-    # Load the PDF content from the specified path
-    pdf = PdfReader(pdf_path)
-    text = ""
-    for page in pdf.pages:
-        text += page.extract_text()
+    # Display the resume text
+    st.subheader("Resume Details")
+    st.write(resume_text)
 
-    # Split the text into chunks
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-    )
-    chunks = text_splitter.split_text(text)
+    # Chatbot conversation loop
+    user_input = st.text_input("You:")
+    chat_history = []
 
-    # Create embeddings
-    embeddings = OpenAIEmbeddings()
-    knowledge_base = FAISS.from_texts(chunks, embeddings)
+    if user_input:
+        chat_history.append(user_input)
+        response = generate_response(user_input)
+        chat_history.append(response)
 
-    # Show user input
-    user_question = st.text_input("Ask a question about your PDF:")
-    if user_question:
-        docs = knowledge_base.similarity_search(user_question)
-
-        llm = OpenAI()
-        chain = load_qa_chain(llm, chain_type="stuff")
-        with get_openai_callback() as cb:
-            response = chain.run(input_documents=docs, question=user_question)
-            print(cb)
-
-        st.write(response)
+        # Display the chat history
+        st.subheader("Chat History")
+        for i in range(0, len(chat_history), 2):
+            st.write("You: " + chat_history[i])
+            st.write("Chatbot: " + chat_history[i + 1])
 
 if __name__ == "__main__":
     main()
