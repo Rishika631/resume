428605b097c1233e976032a2da7126cd6ced774b
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-11 22:03:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
+import spacy
 import re
-import os
-import pandas as pd
 import streamlit as st
-import moviepy.editor as mp
-from youtube_transcript_api import YouTubeTranscriptApi
-from urllib.parse import urlparse, parse_qs
-from transformers import pipeline
-import plotly.express as px
 import openai
+from youtube_transcript_api import YouTubeTranscriptApi
+import moviepy.editor as mp
+import os
+from transformers import pipeline
+from urllib.parse import urlparse, parse_qs
+import speech_recognition as sr
+
+# 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 -new api key - kartik
 
 # Set OpenAI API credentials
 openai.api_key = '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'
@@ -16,11 +18,41 @@ openai.api_key = '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'
 st.set_page_config(page_title="YouTube Video Summarizer and Insights")
 
 # Function to extract transcript from YouTube video
-def extract_transcript(youtube_video):
-    video_id = parse_qs(urlparse(youtube_video).query)['v'][0]
+def extract_transcript_from_youtube(youtube_video):
+    video_id = youtube_video.split("=")[1]
     transcript = YouTubeTranscriptApi.get_transcript(video_id)
-    transcript_text = " ".join([segment['text'] for segment in transcript])
+
+    transcript_text = ""
+    for segment in transcript:
+        transcript_text += segment['text'] + " "
+
     return transcript_text
+
+# Function to extract transcript from video using speech-to-text
+def extract_transcript_from_video(video_path):
+    transcript = ""
+    
+    # Convert video to audio
+    audio_path = "temp_audio.wav"
+    video = mp.VideoFileClip(video_path)
+    video.audio.write_audiofile(audio_path)
+
+    # Use speech recognition to extract transcript
+    r = sr.Recognizer()
+    with sr.AudioFile(audio_path) as source:
+        audio = r.record(source)
+
+    try:
+        transcript = r.recognize_google(audio)
+    except sr.UnknownValueError:
+        st.error("Speech recognition could not understand audio")
+    except sr.RequestError as e:
+        st.error("Could not request results from Google Speech Recognition service: {0}".format(e))
+
+    # Remove temporary audio file
+    os.remove(audio_path)
+
+    return transcript
 
 # Function to summarize transcript using OpenAI's text-davinci-003 model
 def summarize_transcript(transcript):
@@ -35,27 +67,9 @@ def summarize_transcript(transcript):
         presence_penalty=0.0
     )
     summary = response.choices[0].text.strip()
+    return summary
 
-    persons = re.findall(r"\b[A-Z][a-zA-Z]+\b", transcript)  # Extract persons mentioned in the transcript
-    persons = list(set(persons))  # Remove duplicates
-
-    return summary, persons
-
-# Function to calculate contribution based on assigned tasks
-def calculate_contribution(tasks_with_persons):
-    tasks_df = pd.DataFrame(tasks_with_persons)
-    contribution_df = tasks_df["person"].value_counts().reset_index()
-    contribution_df.columns = ["Person", "Tasks Assigned"]
-    total_tasks = contribution_df["Tasks Assigned"].sum()
-    contribution_df["Contribution"] = contribution_df["Tasks Assigned"] / total_tasks * 100
-    return contribution_df
-
-# Function to extract transcript from video
-def extract_transcript_from_video(video_path):
-    transcript = ""
-    
-
-# Function to extract image summary from the video using moviepy
+# Function to extract Image Summary from the video using moviepy
 def extract_image_summary(video_path):
     clip = mp.VideoFileClip(video_path)
     duration = clip.duration
@@ -71,7 +85,7 @@ def extract_image_summary(video_path):
 
     return key_frames, image_summary
 
-# Function to extract action insights & key points from transcript 
+# Function to extract action insights & Key Points from transcript 
 def extract_action_insights(transcript):
     prompt = "Extract action insights and key points both from the following transcript:\n\n" + transcript
     response = openai.Completion.create(
@@ -86,7 +100,6 @@ def extract_action_insights(transcript):
     insights = response.choices[0].text.strip().split("\n")
     return insights
 
-# Function to analyze sentiment of the transcript
 def analyze_sentiment(transcript):
     sentiment_analyzer = pipeline("sentiment-analysis")
     results = sentiment_analyzer(transcript)
@@ -94,7 +107,8 @@ def analyze_sentiment(transcript):
     sentiments = [result["label"] for result in results]
     return sentiments
 
-# Function to extract given task from transcript
+# Function to extract a given task 
+
 def extract_task_from_transcript(transcript, task):
     prompt = f"{task} from the following transcript and mention to whom the task is given:\n\n{transcript}"
     response = openai.Completion.create(
@@ -107,18 +121,11 @@ def extract_task_from_transcript(transcript, task):
         presence_penalty=0.0
     )
     tasks = response.choices[0].text.strip().split("\n")
-    tasks_with_persons = []
-
-    for task in tasks:
-        task_info = task.split(" - ")
-        if len(task_info) == 2:
-            tasks_with_persons.append({"task": task_info[0], "person": task_info[1]})
-
-    return tasks_with_persons
+    return tasks
 
 # Function to perform chatbot interaction
 def chatbot_interaction(transcript, question):
-    # Use OpenAI model API for chatbot interaction
+    # Use LangChain API or any other OpenAI model API for chatbot
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=f"Transcript: {transcript}\nQuestion: {question}",
@@ -148,15 +155,15 @@ def main():
 
         if youtube_video:
             # Extract transcript from YouTube video
-            transcript = extract_transcript(youtube_video)
+            transcript = extract_transcript_from_youtube(youtube_video)
 
             # Summarize transcript
-            summary, persons = summarize_transcript(transcript)
+            summary = summarize_transcript(transcript)
 
             st.info("Meeting processed successfully!")
 
             # Display options
-            options = st.sidebar.multiselect("Select Options:", ["Meeting Summary", "Image Summary", "Action Insights & Key Points", "Sentiment Analysis", "Given Task", "Contribution Chart", "Chatbot"])
+            options = st.sidebar.multiselect("Select Options:", ["Meeting Summary", "Image Summary", "Action Insights & Key Points", "Sentiment Analysis", "Given Task", "Chatbot"])
 
             # Meeting Summary
             if "Meeting Summary" in options:
@@ -192,13 +199,6 @@ def main():
                 tasks_df = pd.DataFrame(tasks_with_persons)
                 st.dataframe(tasks_df)
 
-            # Contribution Chart
-            if "Contribution Chart" in options:
-                st.subheader("Contribution Chart")
-                contribution_df = calculate_contribution(tasks_with_persons)
-                fig = px.pie(contribution_df, values='Contribution', names='Person')
-                st.plotly_chart(fig)
-
             # Chatbot
             if "Chatbot" in options:
                 st.subheader("Chatbot")
@@ -221,7 +221,7 @@ def main():
             transcript = extract_transcript_from_video(video_path)
 
             # Summarize transcript
-            summary, persons = summarize_transcript(transcript)
+            summary = summarize_transcript(transcript)
 
             st.info("Transcript processed successfully!")
 
